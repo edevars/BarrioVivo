c34e25f38e20694054410b18f9ef362d78202482
edevars <e.devars.dev@gmail.com>
2019-12-31 19:19:50 -0600

    :sparkles: Preliminar login form

---
+++ pages/_app.js
@@ -0,0 +1,31 @@
+import App from "next/app";
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalTheme = createGlobalStyle`
+    body{
+        margin: 0;
+        font-family: sans-serif;
+    }
+`;
+
+const theme = {
+  colors: {
+    primary: "#ffb606",
+    dark: "#2a2a2a",
+    light: "#ffffff"
+  }
+};
+
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <ThemeProvider theme={theme}>
+        <GlobalTheme />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    );
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,69 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import logo from "../images/logotipo_BV.png";
+import background from "../images/login_bg.webp";
+
+const HomeWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  z-index: 0;
+  background: #000;
+`;
+
+const BackgroundImage = styled.img`
+  width: 100%;
+  height: 100vh;
+  object-fit: cover;
+  position: absolute;
+  z-index: 0;
+  opacity: 0.6;
+`;
+
+const LoginWrapper = styled.div`
+  background: ${props => props.theme.colors.primary};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  z-index: 1;
+  border-radius: 10px;
+  .login {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
 
 const Home = () => {
-    return ( 
-        "Hello world!"
-     );
-}
- 
+  const handleSubmint = event => {
+    event.preventDefault();
+    console.log("Haciendo login");
+  };
+
+  return (
+    <HomeWrapper>
+      <BackgroundImage src={background} />
+      <LoginWrapper>
+        <h1>Bienvenido</h1>
+        <img src={logo} alt="" />
+        <form action="" onSubmit={handleSubmint} className="login">
+          <label>Username</label>
+          <input type="text" name="username" autoComplete autoFocus />
+          <label>Password</label>
+          <input type="password" name="password" autoComplete />
+          <input type="submit" value="Ingresar" />
+        </form>
+      </LoginWrapper>
+    </HomeWrapper>
+  );
+};
+
 export default Home;
