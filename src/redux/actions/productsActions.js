c12099af8e19e93449c11c8ba2284b3c076955c8
edevars <e.devars.dev@gmail.com>
2020-01-02 12:45:03 -0600

    :zap: Creating fake fetch

--- src/redux/actions/productsActions.js
+++ src/redux/actions/productsActions.js
@@ -3,23 +3,28 @@ import {
   LOADING_PRODUCT,
   ERROR_IN_PRODUCT
 } from "../types/productsTypes";
+import products from "../../Mocks/Products.json";
 
-import productos from "../../../pages/productos";
+function fakeFetch(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(products.data), ms);
+  });
+}
 
-export const getAllPosts = () => async dispatch => {
+export const getAllProducts = () => async dispatch => {
   dispatch({
     type: LOADING_PRODUCT
   });
 
   try {
-    const response = productos;
-
+    const response = await fakeFetch(1500);
+    
     dispatch({
       type: GET_PRODUCTS,
       payload: response
     });
   } catch (error) {
-    console.error("Error", error.message);
+    console.error("Error: ", error.message);
     dispatch({
       type: ERROR_IN_PRODUCT,
       payload: error.message
