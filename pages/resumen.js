9d193bbd9b9136d16dd44463e9ed02fb252f83fc
edevars <e.devars.dev@gmail.com>
2020-01-02 12:45:19 -0600

    :zap: Connecting with redux

--- pages/resumen.js
+++ pages/resumen.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "../src/Components/Layout";
 
+//Redux
+import { connect } from "react-redux";
+import * as productsActions from "../src/redux/actions/productsActions";
 
-const DashboardPage = () => {
+const DashboardPage = props => {
+  const { products, getAllProducts, loading, error } = props;
+
+  useEffect(() => {
+    if (!products.length) {
+      getAllProducts();
+    }
+  }, [products]);
+
   return (
-    <Layout>
-      <h1>Resumen</h1>
-    </Layout>
+    <Layout>{loading ? <h1>Se esta cargando</h1> : <h1>Resumen</h1>}</Layout>
   );
 };
 
-export default DashboardPage;
+const mapStateToProps = reducers => {
+  return reducers.productsReducer;
+};
+
+export default connect(mapStateToProps, productsActions)(DashboardPage);
