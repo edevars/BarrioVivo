8ef95863237055eb9d81606f41afddca52ce04e9
edevars <e.devars.dev@gmail.com>
2020-01-02 10:02:23 -0600

    :sparkles: Adding wrapper

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Navbar from "../Navbar";
+import styled from "styled-components";
 import PropTypes from "prop-types";
+
+const Wrapper = styled.div`
+  padding: 40px 100px;
+`;
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
-      {children}
+      <Wrapper>{children}</Wrapper>
     </>
   );
 };
