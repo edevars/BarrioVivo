a183c06b3294d981862a1af68f3659f631b81cf4
edevars <e.devars.dev@gmail.com>
2020-01-03 13:31:00 -0600

    :lipstick: Changin spacing in layout

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -4,8 +4,7 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 
 const Wrapper = styled.div`
-  padding: 40px 100px;
-  margin-top: 70px;
+  padding: 20px 100px;
   @media screen and (max-width: 720px) {
     padding: 30px 20px;
   }
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -10,8 +10,6 @@ const Nav = styled.nav`
   color: white;
   padding: 15px 0px;
   display: flex;
-  position: fixed;
-  top: 0;
 `;
 
 const MenuWrapper = styled.div`
