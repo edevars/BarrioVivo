95f6945d21a8609b019f10321429d1868eba7974
edevars <e.devars.dev@gmail.com>
2020-01-03 17:00:56 -0600

    :sparkles: Fixing layout

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -4,8 +4,9 @@ import Footer from "../Footer";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
-const Wrapper = styled.div`
+const Wrapper = styled.main`
   padding: 20px 100px;
+  min-height: calc(100vh - 20px);
   @media screen and (max-width: 720px) {
     padding: 30px 20px;
   }
