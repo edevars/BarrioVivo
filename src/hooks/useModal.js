963e33dbbb6ae3ee1bca44ed9810ef685c3336ff
edevars <e.devars.dev@gmail.com>
2020-01-03 21:15:53 -0600

    :sparkles: Adding modal hook

---
+++ src/hooks/useModal.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const useModal = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return { open, handleOpen, handleClose };
+};
+
+export default useModal;
