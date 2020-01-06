d863a70ea9d824f477ddc463b66e4fb88eabbb63
edevars <e.devars.dev@gmail.com>
2020-01-05 23:49:43 -0600

    :fire: Depuring code

--- pages/configuracion.js
+++ pages/configuracion.js
@@ -1,53 +1,32 @@
-import { useEffect } from "react";
+import { Component } from "react";
 import Layout from "../src/Components/Layout";
 import SettingsContent from "../src/Components/Layout/SettingsContent";
 import Loading from "../src/Components/General/Loading";
 import { connect } from "react-redux";
-import { getAllUsers } from "../src/redux/actions/usersActions";
-import { getAllCategories } from "../src/redux/actions/categoriesActions";
 
-const SettingsPage = props => {
-  const {
-    getAllUsers,
-    getAllCategories,
-    categoriesReducer,
-    usersReducer
-  } = props;
+class SettingsPage extends Component {
+  render() {
+    const {
+      categoriesReducer,
+      usersReducer
+    } = this.props;
 
-  const { loading: loadingUsers, users } = usersReducer;
-  const { loading: loadingCategories, categories } = categoriesReducer;
+    const { loading: loadingUsers, users } = usersReducer;
+    const { loading: loadingCategories, categories } = categoriesReducer;
 
-  const isLoading = loadingUsers || loadingCategories;
-
-  useEffect(() => {
-
-    if (!users.length) {
-      getAllUsers();
-    }
-
-    if (!categories.length) {
-      getAllCategories();
-    }
-  }, []);
-
-  return (
-    <Layout>
-      <h1>Configuracion</h1>
-      {isLoading ? <Loading /> : <SettingsContent />}
-    </Layout>
-  );
-};
-
-const mapDispatchToProps = () => {
-  return {
-    getAllUsers,
-    getAllCategories
-  };
-};
+    const isLoading = loadingUsers || loadingCategories;
+    return (
+      <Layout>
+        <h1>Configuracion</h1>
+        {isLoading ? <Loading /> : <SettingsContent />}
+      </Layout>
+    );
+  }
+}
 
 const mapStateToProps = ({ categoriesReducer, usersReducer }) => {
   const reduxStates = { categoriesReducer, usersReducer };
   return reduxStates;
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(SettingsPage);
+export default connect(mapStateToProps, {})(SettingsPage);
