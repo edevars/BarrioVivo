8b3c4654b2e84111a4caf456f6e7174abebcf07d
edevars <e.devars.dev@gmail.com>
2020-01-05 23:36:57 -0600

    :sparkles: Adding loading

--- pages/configuracion.js
+++ pages/configuracion.js
@@ -1,14 +1,53 @@
-import React from "react";
+import { useEffect } from "react";
 import Layout from "../src/Components/Layout";
 import SettingsContent from "../src/Components/Layout/SettingsContent";
+import Loading from "../src/Components/General/Loading";
+import { connect } from "react-redux";
+import { getAllUsers } from "../src/redux/actions/usersActions";
+import { getAllCategories } from "../src/redux/actions/categoriesActions";
 
-const DashboardPage = () => {
+const SettingsPage = props => {
+  const {
+    getAllUsers,
+    getAllCategories,
+    categoriesReducer,
+    usersReducer
+  } = props;
+
+  const { loading: loadingUsers, users } = usersReducer;
+  const { loading: loadingCategories, categories } = categoriesReducer;
+
+  const isLoading = loadingUsers || loadingCategories;
+
+  useEffect(() => {
+
+    if (!users.length) {
+      getAllUsers();
+    }
+
+    if (!categories.length) {
+      getAllCategories();
+    }
+  }, []);
+
   return (
     <Layout>
       <h1>Configuracion</h1>
-      <SettingsContent />
+      {isLoading ? <Loading /> : <SettingsContent />}
     </Layout>
   );
 };
 
-export default DashboardPage;
+const mapDispatchToProps = () => {
+  return {
+    getAllUsers,
+    getAllCategories
+  };
+};
+
+const mapStateToProps = ({ categoriesReducer, usersReducer }) => {
+  const reduxStates = { categoriesReducer, usersReducer };
+  return reduxStates;
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SettingsPage);
